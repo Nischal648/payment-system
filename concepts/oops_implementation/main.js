5964b86d67577befb35d44ad512ce340e57e82e0
Nischal648 <nischalsharma2347@gmail.com>
2026-06-30 22:50:39 +0530

    Refactor saving_account and current_account classes; improve withdrawal logic and add overdraft limit validation

--- concepts/oops_implementation/main.js
+++ concepts/oops_implementation/main.js
@@ -32,13 +32,12 @@ class saving_account extends bank_account{
         this.#balance+=amount;
         console.log(`saving Account ${amount}`)
     };    
-}
 
 //withdraw money from account
-withdraw(amount){
+        withdraw(amount){
     //validation
     if(amount > this.#balance){
-        console.log("saving account:insuffient balance")
+        console.log("saving account:insuffient balance");
     
     return;
    }
@@ -48,6 +47,68 @@ withdraw(amount){
    );
 }
 //return current balance
-getbalance(){
+getbalance() {
     return this.#balance;
 }
+}
+//inheritance + polymorphism
+//currentAccount IS-A bank account
+
+class current_account extends bank_account{
+   #balance;
+   constructor(initialBalance){
+    super();
+    this.#balance=initialBalance;
+   }
+   deposit(amount){
+    if(this.#balance - amount <-5000){
+        console.log("current draft limit reached ")
+   
+    return;
+     };
+     this.#balance-=amount;
+     console.log(`current account: withdrawn ${amount}`)
+}
+    getbalance(){
+        return this.#balance
+
+    }
+}
+//polymorphism
+//atm does not know -> savingAccount and currentaccount 
+//atm only knows this objects supports :
+//withdraw(),getbalance()
+
+//that is the power of abstraction 
+
+class atm{
+    withdrawMoney(account,amount){
+        console.log("processing withdrawal")
+        account.withdraw(amount);
+        console.log("remaining balance",account.getBalance());
+    }
+    depositMoney(account,amount){
+            console.log("processing withdrawal")
+            account.deposit(amount);
+            console.log("updated balane",account.getBalance());
+    }
+}
+
+//main program 
+//create saving account 
+const saving  = new saving_account(10000);
+
+//create current account 
+const current = new current_account(100000);
+
+//create atm
+const atm1 = new atm();
+
+//deposit into the saving account
+atm1.depositMoney("saving",2000);
+
+//withdraw from saving account
+atm1.withdrawMoney(current,10000);
+
+//withdraw beyond overdraft limit
+atm1.withdraw(current,10000)
